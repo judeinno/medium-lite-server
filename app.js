2f7d58beb74a2c01544e76a611185e395b8db09a
Atuhaire Innocent <Atuhaire.Innocent_contractor@LevelTenEnergy.com@Atuhaires-MacBook-Pro.local>
2023-08-15 12:08:53 -0500

    Enable cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const userRoutes = require('./src/routes/userRoutes')
 const blogRoutes = require('./src/routes/blogRoutes')
+const cors = require('cors');
 require("dotenv").config();
 const dbConnection = require('./conn')
 
@@ -19,3 +20,7 @@ app.listen(port, () => {
 
 app.use('/user', userRoutes)
 app.use('/blog', blogRoutes)
+
+app.use(cors({
+  origin: '*'
+}));
